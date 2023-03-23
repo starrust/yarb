8b4a6f7ad187946e6174efb22ee6c88ce356cbaa
starrust <50724910+starrust@users.noreply.github.com>
2023-03-23 09:28:41 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -105,7 +105,7 @@ class wecomBot:
                 else:
                     Color.print_failed('[-] wecomBot 发送失败')
                     print(r.text)
-            time.sleep(1)
+            time.sleep(3)
 
 
 class dingtalkBot:
